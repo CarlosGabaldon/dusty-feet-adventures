ce91519c6e2a030cafb131ff05fc206084174725
CarlosGabaldon <cgabaldon@gmail.com>
2015-11-17 08:53:02 -0700

    Need trails list to verify that the home page returns a list of trails

--- spec/features/listing_trails_spec.rb
+++ spec/features/listing_trails_spec.rb
@@ -1,8 +1,19 @@
 require "rails_helper"
 
 RSpec.feature "Users can view trail listings" do
+  before do
+    #refactor this ugly code to be more Ruby...
+    @trails = []
+    @trail_1 = FactoryGirl.create(:trail)
+    @trail_2 = FactoryGirl.create(:trail, name: "Circle Trail #332",
+      description: "Circle Trail #332 rises and falls over the hilly terrain of Arizona’s central highlands.")
+    @trails << @trail_1
+    @trails << @trail_2
+  end
+
   scenario "when visting the home page" do
     visit "/"
+    #expect to include @trails when visting the home page as collection
     expect(page).to have_content "Trails in your area"
   end
 end
