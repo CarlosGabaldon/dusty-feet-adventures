2084c107e2b1aaa094f85dc55ee942ae44622870
CarlosGabaldon <cgabaldon@gmail.com>
2015-11-19 14:47:38 -0700

    Users should be able to edit a trail

---
+++ spec/features/editing_trails_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.feature "Users can edit trails" do
+  before do
+    @trail = FactoryGirl.create(:trail_one)
+  end
+  scenario "with valid attributes" do
+    visit "/"
+    click_link @trail.name
+    click_link "Edit Trail"
+
+    fill_in "Name", with: "Trail one updated"
+    fill_in "Description", with: "Trail one description updated."
+    click_button "Update Trail"
+
+    expect(page).to have_content "Trail Updated."
+    expect(page).to have_content "Trail one updated"
+  end
+
+end
