6718db86ba48775d32fc14715bdb6c5b65f51fc6
CarlosGabaldon <cgabaldon@gmail.com>
2015-11-21 08:26:47 -0700

    Trail should be valid with name and description, but invalid without a name

--- spec/models/trail_spec.rb
+++ spec/models/trail_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Trail, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with name and description" do
+    trail = FactoryGirl.create(:trail_one)
+    expect(trail).to be_valid
+  end
+
+  it "is invalid with name missing" do
+    trail = FactoryGirl.build(:trail_missing_name)
+    trail.valid?
+    expect(trail.errors[:name]).to include("can't be blank")
+  end
 end
