d8fc5b2eafab36af278a170a2616a8871c905818
CarlosGabaldon <cgabaldon@gmail.com>
2015-11-23 07:43:10 -0700

    Users can cancel when editing a trail

--- spec/features/editing_trails_spec.rb
+++ spec/features/editing_trails_spec.rb
@@ -3,18 +3,29 @@ require 'rails_helper'
 RSpec.feature "Users can edit trails" do
   before do
     @trail = FactoryGirl.create(:trail_one)
-  end
-  scenario "with valid attributes" do
     visit "/"
     click_link @trail.name
     click_link "Edit Trail"
+  end
 
+  scenario "with valid attributes" do
     fill_in "Name", with: "Trail three"
     fill_in "Description", with: "Trail one description updated."
     click_button "Update Trail"
 
     expect(page).to have_content "Trail Updated."
     expect(page).to have_content "Trail three"
+  end
+
+  scenario "with invalid attributes" do
+    fill_in "Name", with: ""
+    click_button "Update Trail"
+    expect(page).to have_content "Name can't be blank"
+  end
+
+  scenario "with when clicking cancel return to trail listing" do
+    click_link "Cancel"
+    expect(page.current_url).to eq trail_url(@trail)
   end
 
 end
