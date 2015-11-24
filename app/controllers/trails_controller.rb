219494b0946dc03eaf5765a76c15ae83727eb5b1
CarlosGabaldon <cgabaldon@gmail.com>
2015-11-24 11:54:56 -0700

    Display a static google map for trails#show page

--- app/controllers/trails_controller.rb
+++ app/controllers/trails_controller.rb
@@ -5,6 +5,7 @@ class TrailsController < ApplicationController
 
   def show
     @trail = Trail.find(params[:id])
+    @map_uri = build_map_uri(@trail.location.geocode)
   end
 
   def new
@@ -44,6 +45,19 @@ class TrailsController < ApplicationController
   def trail_params
     params.require(:trail).permit(:name, :description,
       location_attributes: [:id, :geocode])
+  end
+
+  def build_map_uri(geocode)
+    url = Rails.configuration.x.google_maps_static_api
+    api_key = "key=#{Rails.configuration.x.google_api_key}"
+    center = "center=#{geocode}"
+    zoom = "zoom=14&scale=false&size=600x300"
+    map_type = "maptype=roadmap"
+    format = "format=png"
+    visual_refresh = "visual_refresh=true"
+    markers = "markers=size:mid%7Ccolor:0xff0000%7Clabel:1%7C#{geocode}"
+
+    "#{url}#{center}&#{zoom}&#{map_type}&#{format}&#{visual_refresh}&#{markers}&#{api_key}"
   end
 
 end
---
+++ config/initializers/dustyfeetadventures.rb
@@ -0,0 +1,2 @@
+DustyFeetAdventures::Application.config.x.google_maps_static_api = "http://maps.googleapis.com/maps/api/staticmap?"
+DustyFeetAdventures::Application.config.x.google_api_key = ENV["google_api_key"]
