98cb1e56361b172f9c9091a8e9ba0af0f91fc7f2
CarlosGabaldon <cgabaldon@gmail.com>
2015-11-26 09:49:39 -0700

    Users can view all trails from trail detail page

--- spec/features/showing_trails_spec.rb
+++ spec/features/showing_trails_spec.rb
@@ -3,16 +3,20 @@ require 'rails_helper'
 RSpec.feature "Users should be able to view trail details" do
   before do
     @trail = create(:trail_one)
-  end
-  scenario "when visting the show page" do
     visit '/'
     click_link @trail.name
-
+  end
+  scenario "when visting the show page" do
     expect(page).to have_content @trail.name
     expect(page).to have_content @trail.description
     expect(page).to have_content @trail.location.geocode
     expect(page).to have_css "img[id*='map_#{@trail.id}']"
     expect(page).to have_css "a[href*='#{@trail.location.geocode}']"
+  end
+
+  scenario "when clicking 'all' view trail listing page" do
+    click_link "All Trails"
+    expect(page.current_url).to eq trails_url
   end
 
 end
