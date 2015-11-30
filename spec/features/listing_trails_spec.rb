9d18ad6f03d250331e811fae0786dd5cf6e4abc1
CarlosGabaldon <cgabaldon@gmail.com>
2015-11-29 18:20:11 -0700

    Trail listing page will show the first image if it exists

--- spec/features/listing_trails_spec.rb
+++ spec/features/listing_trails_spec.rb
@@ -2,14 +2,14 @@ require "rails_helper"
 
 RSpec.feature "Users can view trail listings" do
   before do
-    @trails = [create(:trail_one), create(:trail_two)]
+    @trails = [create(:trail_with_images), create(:trail_two)]
   end
 
   scenario "when visting the home page" do
     visit "/"
     expect(page).to have_content "Trails"
     expect(page).to have_selector "li", count: 2
-    expect(page).to have_content @trails[0].name
-    expect(page).to have_content @trails[0].location.geocode
+    expect(page).to have_content @trails.first.name
+    expect(page).to have_css "img[src='#{@trails.first.images.first.url}']"
   end
 end
