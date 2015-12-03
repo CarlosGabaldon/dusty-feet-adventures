88ba6dcd4a5208acdbef7d713e2273662c26ba6a
CarlosGabaldon <cgabaldon@gmail.com>
2015-12-02 17:56:13 -0700

    Basic implementation of creating a trail path map for a specific trail

--- app/controllers/trails_controller/google_maps.rb
+++ app/controllers/trails_controller/google_maps.rb
@@ -1,14 +1,16 @@
 class TrailsController
   class GoogleMaps
     attr_reader :parameters
-    attr_reader :geocode
+    attr_reader :markers_lat_long_coords
+    attr_reader :path_lat_long_coords
 
-    def initialize(geocode)
-      @geocode = geocode
+    def initialize(markers_lat_long_coords, path_lat_long_coords="")
+      @markers_lat_long_coords = markers_lat_long_coords
+      @path_lat_long_coords = path_lat_long_coords
       @parameters = {
         url: Rails.configuration.x.google_maps_static_api,
         api_key: "key=#{Rails.configuration.x.google_api_key}",
-        center: "center=#{@geocode}",
+        center: "center=#{@markers_lat_long_coords}",
         autoscale: "autoscale=false",
         size: "size=600x300",
         zoom: "zoom=14",
@@ -16,21 +18,33 @@ class TrailsController
         map_type: "maptype=hybrid",
         format: "format=png",
         visual_refresh: "visual_refresh=true",
-        markers: "markers=size:mid%7Ccolor:0xff0000%7Clabel:1%7C#{@geocode}",
+        markers: "markers=size:mid%7Ccolor:0xff0000%7Clabel:1%7C#{@markers_lat_long_coords}",
+        path: "path=color:0x0000ff|weight:5|#{@path_lat_long_coords}"
 
       }
 
     end
 
-    def static_map_uri
+    def map_with_marker_uri
       uri = "#{@parameters[:url]}#{@parameters[:autoscale]}&#{@parameters[:size]}"
       uri << "&#{@parameters[:map_type]}&#{@parameters[:format]}"
       uri << "&#{@parameters[:visual_refresh]}&#{@parameters[:markers]}"
       uri << "&#{@parameters[:api_key]}"
     end
 
-    def maps_url
-      "http://maps.google.com/?q=#{@geocode}"
+    def map_with_path_uri
+      uri = "#{@parameters[:url]}#{@parameters[:autoscale]}&#{@parameters[:size]}"
+      uri << "&#{@parameters[:map_type]}&#{@parameters[:format]}"
+      uri << "&#{@parameters[:visual_refresh]}&#{@parameters[:markers]}"
+      uri << "&#{@parameters[:path]}&#{@parameters[:api_key]}"
+    end
+
+    def map_url
+      "http://maps.google.com/?q=#{@markers_lat_long_coords}"
+    end
+
+    def map_with_path_url
+
     end
   end
 end
--- spec/features/creating_trails_spec.rb
+++ spec/features/creating_trails_spec.rb
@@ -9,6 +9,7 @@ RSpec.feature "Users can create trails" do
     fill_in "Name", with: "Bright Angel Trail"
     fill_in "Description", with: "A very busy trail that runs to the river."
     fill_in "Geocode", with: "34.575597, -112.427461"
+    fill_in "Path", with: "34.549089, -112.537448|34.551259, -112.535793"
     click_button "Add Trail"
 
     expect(page).to have_content "Trail Added."
