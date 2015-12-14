f778377b255ed78d1f1c725a34eb8c5f8cfeea9f
CarlosGabaldon <cgabaldon@gmail.com>
2015-12-14 10:54:28 -0700

    New scenario to verify that users can add a new gpx file to an existing trail

--- spec/features/editing_trails_spec.rb
+++ spec/features/editing_trails_spec.rb
@@ -24,6 +24,12 @@ RSpec.feature "Users can edit trails" do
     expect(page).to have_content "Name can't be blank"
   end
 
+  scenario "with updating a gpx file" do
+    attach_file "gpx_file", "#{Rails.root}/spec/fixtures/trail_gpx.xml"
+    click_button "Update Trail"
+    expect(page).to have_content "Trail Updated"
+  end
+
   scenario "with when clicking cancel return to trail detail" do
     click_link "Cancel"
     expect(page.current_url).to eq trail_url(@trail)
