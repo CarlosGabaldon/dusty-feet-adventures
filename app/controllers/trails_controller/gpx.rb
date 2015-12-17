d0b769e6187567b80e1fff09c733cb62c0fd57b4
CarlosGabaldon <cgabaldon@gmail.com>
2015-12-16 18:43:25 -0700

    Parse lat/lon coords from trkpt gpx elements to build route as '|' seperated coords

--- app/controllers/trails_controller/gpx.rb
+++ app/controllers/trails_controller/gpx.rb
@@ -13,16 +13,16 @@ class TrailsController
 
     private
     def parse_file
-      route = "34.549009, -112.536735|"
-      route << "34.550168, -112.536752|"
-      route << "34.550480, -112.536408|"
-      route << "34.551081, -112.535954|"
-      route << "34.551267, -112.535701|"
-      route << "34.551143, -112.535205|"
-      route << "34.550753, -112.535149|"
-      route << "34.550178, -112.535351|"
-      route << "34.549646, -112.536022|"
-      route << "34.549009, -112.536735"
+      route = ""
+      if @file_path
+        doc = Nokogiri::XML(File.open(@file_path))
+        trkpts = doc.xpath("//xmlns:trkpt")
+        trkpts.each do |trkpt|
+          route << "#{trkpt.attr('lat')}, #{trkpt.attr('lon')}|"
+        end
+        route = route[0...-1]
+      end
+      return route
     end
   end
 end
