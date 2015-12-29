def3153cde78d8a9706b14ebad3002694b53dc17
CarlosGabaldon <cgabaldon@gmail.com>
2015-12-29 11:27:45 -0700

    Add seed data for users and trails

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+## User Data ##
+unless User.exists? email: "admin@parametertechnologies.com"
+  User.create! email: "admin@parametertechnologies.com", password: "password", admin: true
+end
+
+unless User.exists? email: "viewer@parametertechnologies.com"
+  User.create! email: "viewer@parametertechnologies.com", password: "password"
+end
+
+## Trail Data ##
+unless Trail.exists? name: "Bright Angel Trail"
+  Trail.create! name: "Bright Angel Trail",
+    description: "A very busy trail that runs to the river.",
+    location_attributes: {lat_long_coords:  "34.575597, -112.427461", state: "AZ" }
+end
