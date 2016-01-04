ed2c9b90d436175e64279c958651280803133be8
CarlosGabaldon <cgabaldon@gmail.com>
2016-01-04 14:07:49 -0700

    Do not use Sauce labs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,18 +5,6 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
-
-if ENV['TRAVIS']
-  require "selenium-webdriver"
-  caps = Selenium::WebDriver::Remote::Capabilities.firefox({
-  'tunnel-identifier' => ENV['TRAVIS_JOB_NUMBER']
-  })
-  driver = Selenium::WebDriver.for(:remote, {
-    url: 'http://carlosgabaldon:e11459bc-68d0-40a6-a5f7-c4452b97e0de@ondemand.saucelabs.com/wd/hub',
-    desired_capabilities: caps
-  })
-end
-
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
