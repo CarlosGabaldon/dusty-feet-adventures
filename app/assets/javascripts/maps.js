07569d2cf83df962e18f6fe3da422ac1f755b7ce
CarlosGabaldon <cgabaldon@gmail.com>
2016-01-05 17:11:23 -0700

    Have basic implementation working displaying google map based on Trail#location.lat_long_coords and Trail#route, both lat_long_coords and route hold JSON objects that are stored on the map div on ./app/views/trails/show.html.erb. I am using seeds to create the correct string JSON objects for the first seeded trail. Next iteration I need to update the GPX trail parsing to create the correct string format in gpx.rb. Also, I need to figure out how to create a second google map for the trail location map, then I can stop using the Google Maps static api

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,44 +1,32 @@
-
-
-// var map;
-// var coords;
-// function initMap() {
-//   coords = $("#map").data("params");
-//   alert(coords);
-//   map = new google.maps.Map(document.getElementById('map'), {
-//     center: {lat: -34.397, lng: 150.644},
-//     zoom: 8
-//   });
-// }
-
+//
+// Used to draw the google map based on:
+//  - Location of the trail: Trail#location.lat_long_coords
+//  - Route of the trail: Trail#Route
+//  The above values are stored in data param values in the view:
+//  - ./app/views/trails/show.html.erb
+//
 function initMap() {
-    if ($("#map").length) {
-    var coords = $("#map").data("route");
-    var location = $("#map").data("location")
-    // alert(coords);
-    // alert(location);
+  if ($("#map").length) {
+    var route = $("#map").data("route");
+    var location = $("#map").data("location");
     var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 5,
-      center: {lat: 24.886, lng: -70.268},
-      mapTypeId: google.maps.MapTypeId.TERRAIN
+      zoom: 17,
+      center: location,
+      mapTypeId: google.maps.MapTypeId.HYBRID
     });
 
-    // Define the LatLng coordinates for the polygon's path.
-    var route_coords = [
-      {lat: 25.774, lng: -80.190},
-      {lat: 18.466, lng: -66.118},
-      {lat: 32.321, lng: -64.757},
-      {lat: 25.774, lng: -80.190}
-    ];
+    var marker = new google.maps.Marker({
+      position: location,
+      map: map,
+      title: 'Trail Head'
+    });
 
     // Construct the polygon.
     var trail_route = new google.maps.Polygon({
-      paths: route_coords,
+      paths: route,
       strokeColor: '#FF0000',
       strokeOpacity: 0.8,
-      strokeWeight: 2,
-      fillColor: '#FF0000',
-      fillOpacity: 0.35
+      strokeWeight: 2
     });
     trail_route.setMap(map);
   }
