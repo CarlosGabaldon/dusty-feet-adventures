45e023bbf7620b52f75c5bb0944fc6adf99a410d
CarlosGabaldon <cgabaldon@gmail.com>
2016-01-09 18:36:13 -0700

    Additional test coverage for Trails controller

--- spec/controllers/trails_controller_spec.rb
+++ spec/controllers/trails_controller_spec.rb
@@ -27,12 +27,40 @@ RSpec.describe TrailsController, type: :controller do
   end
 
   context 'non-admin users' do
-    it 'are not allowed to access new action' do
-      get :new
+    describe 'GET new' do
+      it 'are not allowed to access new action' do
+        get :new
 
-      expect(response).to redirect_to '/'
-      expect(flash[:alert]).to eq 'You must be an admistrator to access.'
+        expect(response).to redirect_to '/'
+        expect(flash[:alert]).to eq 'You must be an admistrator to access.'
+      end
     end
   end
 
+  context "admin-users" do
+    before do
+      user = create(:user, admin: true)
+      allow(controller).to receive(:authenticate_user!)
+      allow(controller).to receive(:current_user).and_return(user)
+    end
+
+    describe 'GET new' do
+      it 'assigns @trail' do
+        get :new
+
+        expect(assigns(:trail))
+        expect(response).to render_template('new')
+      end
+    end
+
+    describe 'GET edit' do
+      it 'assigns @trail' do
+        trail = create(:trail_one)
+        get :edit, params: {id: trail.id }
+
+        expect(assigns(:trail)).to eq trail
+        expect(response).to render_template('edit')
+      end
+    end
+  end
 end
