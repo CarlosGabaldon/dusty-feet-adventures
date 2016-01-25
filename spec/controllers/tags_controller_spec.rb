c7beb31aeb52b5a070fac058ea84375ab7873b3b
CarlosGabaldon <cgabaldon@gmail.com>
2016-01-25 12:44:53 -0700

    Add example for tags controller

--- spec/controllers/tags_controller_spec.rb
+++ spec/controllers/tags_controller_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe TagsController, type: :controller do
+  let(:trail) {create(:trail_with_tag_names)}
+
+  describe "DELETE #remove" do
+    it "returns http success" do
+      expect{
+        delete :remove, params: { trail_id: trail, id: trail.tags.first.id }
+      }.to change(trail.tags, :count).by(-1)
+
+      expect(response).to have_http_status(:success)
+    end
+  end
 
 end
